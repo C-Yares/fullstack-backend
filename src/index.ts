5ee05105d99a8f3325d04195d9cb2cf0430a2d7e
C-Yares <carl.yaores@gmail.com>
2025-07-04 21:58:54 +0800

    Add role router

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,14 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import prisma from "./prismaClient";
+import rolesRouter from "./routes/roles";
 
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 
+app.use(rolesRouter);
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    console.log('Got request')
-    res.send('I got your request')
-})
-app.get('/roles', async(req: express.Request, res: express.Response)=>{
-    const roles = await prisma.role.findMany({
-        include: {
-            permissions: true,
-        }
-    })
-    res.json(roles);
-})
 app.listen(8888, () => {
     console.log('Server is running on port 8888')
 })
