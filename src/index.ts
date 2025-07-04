91b536c971c23c4977c7eb8768412bc0a72b1d2b
C-Yares <carl.yaores@gmail.com>
2025-07-05 00:19:45 +0800

    Add user login & authentication

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import prisma from "./prismaClient";
 import rolesRouter from "./routes/roles";
 import usersRouter from "./routes/users";
+import authRouter from "./routes/auth";
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 
 app.use(rolesRouter);
 app.use(usersRouter);
+app.use(authRouter);
 
 app.listen(8888, () => {
     console.log('Server is running on port 8888')
---
+++ src/scripts/config.ts
@@ -0,0 +1,5 @@
+const CONFIG = {
+    JWT_SECRET: 'secret', //use env variable in production
+}
+
+export default CONFIG;
