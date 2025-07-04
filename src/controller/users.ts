ca10c4950f0101f0041458d6aa4c01d49d2769a9
C-Yares <carl.yaores@gmail.com>
2025-07-04 23:11:54 +0800

    add get user by id

--- src/controller/users.ts
+++ src/controller/users.ts
@@ -24,10 +24,22 @@ export const createUser = async (request: Request, response: Response) => {
 }
 
 export const listUsers = async (request: Request, response: Response) => {
-    const users = await prisma.user.findMany({
-        include: {
-            role: true
+    const users = await prisma.user.findMany();
+    response.json(users);
+}
+
+export const getUser = async (request: Request, response: Response) => {
+    const { id } = request.params;
+    const user = await prisma.user.findUnique({
+        where:{
+            id:Number(id)
         }
     });
-    response.json(users);
+
+    if(!user){
+        response.status(404).json({error: "User not found"});
+        return;
+    }
+
+    response.json(user);
 }
